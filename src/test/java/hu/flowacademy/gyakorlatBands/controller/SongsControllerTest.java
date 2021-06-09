341de0b527d04501c5eab960b24c4dbbea01c238
Vass Tamás <vass.tamas87@gmail.com>
2021-06-09 15:02:56 +0200

    songsControllerTest modified

--- src/test/java/hu/flowacademy/gyakorlatBands/controller/SongsControllerTest.java
+++ src/test/java/hu/flowacademy/gyakorlatBands/controller/SongsControllerTest.java
@@ -1,23 +1,67 @@
 package hu.flowacademy.gyakorlatBands.controller;
 
+import hu.flowacademy.gyakorlatBands.enumPackage.Genre;
 import hu.flowacademy.gyakorlatBands.model.Album;
+import hu.flowacademy.gyakorlatBands.model.Band;
+import hu.flowacademy.gyakorlatBands.repository.AlbumRepository;
+import hu.flowacademy.gyakorlatBands.repository.BandsRepository;
+import hu.flowacademy.gyakorlatBands.repository.SongsRepository;
 import io.restassured.http.ContentType;
 import hu.flowacademy.gyakorlatBands.model.Songs;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.when;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static io.restassured.RestAssured.given;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class SongsControllerTest {
 
-    public Album album = Album.builder().id(2).build();
+    @Autowired
+    BandsRepository bandsRepository;
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    SongsRepository songsRepository;
+
+    private void insert() {
+        var saveBand = bandsRepository.saveAll(List.of(
+                Band.builder().name("Beatles").genre(Genre.POP).build(),
+                Band.builder().name("Eminem").genre(Genre.RAP).build(),
+                Band.builder().name("Korn").genre(Genre.ROCK).build(),
+                Band.builder().name("Slipknot").genre(Genre.ROCK).build()
+        ));
+
+        var saveAlbums = albumRepository.saveAll(List.of(
+                Album.builder().title("Album cím").releaseDate("1999").band(saveBand.get(0)).build(),
+                Album.builder().title("Album cím2").releaseDate("2001").band(saveBand.get(0)).build(),
+                Album.builder().title("Album cím3").releaseDate("2004").band(saveBand.get(0)).build(),
+                Album.builder().title("Album cím4").releaseDate("2007").band(saveBand.get(1)).build(),
+                Album.builder().title("Album cím5").releaseDate("2010").band(saveBand.get(1)).build()
+        ));
+
+        songsRepository.saveAll(List.of(
+                Songs.builder().songTitle("Szám címe").length(3.4).licencePrice(2000).album(saveAlbums.get(0)).build(),
+                Songs.builder().songTitle("Szám címe2").length(5.6).licencePrice(3500).album(saveAlbums.get(0)).build(),
+                Songs.builder().songTitle("Szám címe3").length(2.6).licencePrice(1500).album(saveAlbums.get(1)).build()
+        ));
+    }
 
     @Test
     public void testCreateSong() {
-        createSong(album);
+        insert();
+        createSong(albumRepository.findAll().get(0));
     }
 
     private Songs createSong(Album album) {
@@ -43,8 +87,17 @@ class SongsControllerTest {
     }
 
     @Test
+    public void testGetAllSongs() {
+        insert();
+        when().get("/api/songs").then()
+                .statusCode(200)
+                .body("size()", is(3));
+    }
+
+    @Test
     public void testDeleteSong() {
-        Songs song = createSong(album);
+        insert();
+        Songs song = createSong(albumRepository.findAll().get(0));
         given()
                 .when()
                 .delete("/api/songs/" + song.getId())
